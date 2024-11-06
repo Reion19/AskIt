d9a2aa884533b6e759de91a5608f4ba90eac754a
Reion19 <reizu1906@gmail.com>
2024-11-06 19:22:58 +0000

    First Commit

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,44 @@
+class AnswersController < ApplicationController
+  before_action :set_question!
+
+  def create
+    @answer = @question.answers.build answer_params
+
+    if @answer.save
+      flash[:success] = "Answer created!"
+      redirect_to question_path(@question)
+    else
+      @answers = Answer.order created_at: :desc
+      render 'questions/show'
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    answer = @question.answers.find params[:id]
+    answer.destroy
+    flash[:success] = "Answer deleted!"
+    redirect_to question_path(@question)
+  end
+
+  def update
+    if @answer.update answer_params
+      flash[:success] = "Answer updated!"
+      redirect_to question_path(@question)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+
+  def set_question!
+    @question = Question.find params[:question_id]
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :questions do
+    resources :answers, only: %i[create destroy update]
+  end
+
+  root "pages#index"
+end
