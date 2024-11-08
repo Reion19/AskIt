6d9e2ecca0f5bf20476afb3ad6046e5b28c87d87
Reion19 <reizu1906@gmail.com>
2024-11-08 18:58:22 +0000

    Added a lot of thing, i can't remember them all

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
+  include Pagy::Backend
   include ErrorHandling
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]).decorate if session[:user_id].present?
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+
+  helper_method :current_user, :user_signed_in?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_06_171544) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_08_142255) do
   create_table "answers", force: :cascade do |t|
-    t.text "body"
+    t.text "body", null: false
     t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -20,10 +20,19 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_06_171544) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
