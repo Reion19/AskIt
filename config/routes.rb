126da23ec04e757db9c09180572e06fb0bf10461
Reion19 <reizu1906@gmail.com>
2024-12-23 16:02:00 +0000

    3rd video

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :questions # the same thing as line below
+  # resources :questions, only: %i[index new edit create update destroy show]
+
   root "pages#index"
 
-  get "/questions", to: "questions#index"
+  # get "/questions", to: "questions#index"
+  # get "/questions/new", to: "questions#new"
+  # post "/questions", to: "questions#create"
+  # get "questions/:id/edit", to: "questions#edit" => will be created with resources method
 end
