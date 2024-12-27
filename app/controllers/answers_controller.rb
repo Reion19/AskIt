dac85df349049147532a79f56235043f93cff7a7
Reion19 <reizu1906@gmail.com>
2024-12-27 19:42:49 +0200

    5th lesson

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,15 +1,23 @@
 class AnswersController < ApplicationController
-  before_action :set_question!, only: %i[create]
+  before_action :set_question!, only: %i[create destroy]
 
   def create
     @answer = @question.answers.build answer_params
 
     if @answer.save
-      flash[:success] = "Answer created!"
+      flash[:success] = 'Answer created!'
       redirect_to question_path(@question.id)
     else
-      render "questions/show"
+      @answers = Answer.order created_at: :desc
+      render 'questions/show'
     end
+  end
+
+  def destroy
+    @answer = @question.answers.find_by(params[:answer_id])
+    @answer.destroy
+    flash[:success] = 'Answer deleted!'
+    redirect_to question_path(id: @question.id)
   end
 
   private
