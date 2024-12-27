3e228b8e4530e835ee7d1b21a47cc058c8b93e2f
Reion19 <reizu1906@gmail.com>
2024-12-27 20:17:59 +0200

    lesson 5 homework

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,6 @@
 class AnswersController < ApplicationController
-  before_action :set_question!, only: %i[create destroy]
+  before_action :set_question!, only: %i[create destroy edit]
+  before_action :set_answer!, only: %i[destroy edit update]
 
   def create
     @answer = @question.answers.build answer_params
@@ -14,16 +15,33 @@ class AnswersController < ApplicationController
   end
 
   def destroy
-    @answer = @question.answers.find_by(params[:answer_id])
     @answer.destroy
     flash[:success] = 'Answer deleted!'
     redirect_to question_path(id: @question.id)
+  end
+
+  def edit
+  end
+
+  def update
+    if @answer.update answer_params
+      flash[:success] = 'Answer updated!'
+      redirect_to question_path(@question.id)
+    else
+      @answers = Answer.order created_at: :desc
+      render 'questions/show'
+    end
   end
 
   private
 
   def answer_params
     params.require(:answer).permit(:body)
+  end
+
+  def set_answer!
+    set_question!
+    @answer = @question.answers.find_by(params[:answer_id])
   end
 
   def set_question!
