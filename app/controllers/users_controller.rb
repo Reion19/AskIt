a5b6af4e7dfb6564b693b4474b0237c9bf8650d0
Reion19 <reizu1906@gmail.com>
2025-02-25 11:17:42 +0200

    user sign up functional -> implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = "Welcome to AskIt, #{@user.name}"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
 end
